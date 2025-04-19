04ac315e9a090cffe2fe3e0312e2617d2b81cc18
jo-eun-chae <dmsco9835@gmail.com>
2025-04-19 23:54:37 +0900

    feat : 20250411_(task proj) board list, side form, list, tack, action button, drop down form

--- 07_frontend_advanced/react-task-app/src/components/BoardList/BoardList.tsx
+++ 07_frontend_advanced/react-task-app/src/components/BoardList/BoardList.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { FC, useState } from 'react'
+import { useTypedSelector } from '../../hooks/redux';
+import SideForm from './SideForm/SideForm';
+import { FiPlusCircle } from 'react-icons/fi';
 
-const BoardList = () => {
+type TBoardListProps = {
+  activeBoardId: string
+  setActiveBoardId: React.Dispatch<React.SetStateAction<string>>
+};
+
+const BoardList: FC<TBoardListProps> = ({
+  activeBoardId,
+  setActiveBoardId
+}) => {
+  const { boardArray } = useTypedSelector(state => state.boards);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
   return (
     <div>
-      
+      <div>
+        게시판:
+      </div>
+      {boardArray.map((board, index) => (
+        <div key={board.boardId}>
+          <div>
+            {board.boardName}
+          </div>
+        </div>
+      ))}
+      <div>
+        {
+          isFormOpen ? 
+            <SideForm setIsFormOpen={setIsFormOpen}/>
+            :
+            <FiPlusCircle onClick={() => setIsFormOpen(!isFormOpen)}/>
+        }
+      </div>
     </div>
   )
 }
--- 07_frontend_advanced/react-task-app/src/components/BoardList/SideForm/SideForm.tsx
+++ 07_frontend_advanced/react-task-app/src/components/BoardList/SideForm/SideForm.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SideForm = () => {
   return (
     <div>
-      
+      SideForm
     </div>
   )
 }
